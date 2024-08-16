29f5fcd0389adc1f146b12180b5d442a19b410d2
Rohit Kotian <rohitkotian07@gmail.com>
2024-08-16 09:06:03 +0530

    Additional fixes

--- src/components/UserMessage.jsx
+++ src/components/UserMessage.jsx
@@ -3,8 +3,7 @@ import userIcon from "../assets/user-icon.png";
 export default function UserMessage({ message }) {
   return (
     <div className="flex justify-end">
-      <div className="flex w-fit bg-slate-500 mb-3 px-4 py-3 text-sm rounded-md items-center">
-        <img className="mr-4 w-8 h-fit" src={userIcon} alt="user-logo" />
+      <div className="w-fit bg-slate-500 mb-3 px-4 py-3 text-sm rounded-md items-center">
         <p>{message.text}</p>
       </div>
     </div>
