5d64c610a6f575fe858c10a64f66a4f2c9a55fff
Rohit Kotian <rohitkotian07@gmail.com>
2024-10-18 08:45:33 +0530

    Show password button added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,6 +5,7 @@ import ErrorMessage from "./ErrorMessage";
 
 export default function Login() {
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
   const loginInitialForm = {
     email: "",
@@ -46,15 +47,29 @@ export default function Login() {
         </div>
         <div className="mb-5">
           <label className="block text-md font-medium">Password</label>
-          <input
-            onChange={(e) =>
-              setFormData({ ...formData, password: e.target.value })
-            }
-            className="w-full p-2 rounded-md outline-none bg-slate-300"
-            type="password"
-            placeholder="Enter Password"
-            required
-          />
+          <div className="flex p-2 rounded-md bg-slate-300 items-center">
+            <input
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+              className="w-full outline-none bg-slate-300"
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter Password"
+              required
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="#606060"
+              className="bi bi-eye-fill cursor-pointer"
+              viewBox="0 0 16 16"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            >
+              <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+              <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
+            </svg>
+          </div>
         </div>
         <button className="w-full bg-blue-950 text-white p-2 mb-1">
           Login
